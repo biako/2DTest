7b40d3cf2e2feab8ddd9c220c17cd0f4f2686fab
biako <ryushoryu@163.com>
2017-04-08 15:46:09 +0800

    04/08/2017 Added the menu

---
+++ Assets/Scripts/SceneManager/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void StartMainMenu() {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void StartRcoketWar() {
+        SceneManager.LoadScene("RocketWar", LoadSceneMode.Single);
+    }
+
+    public void StartAnroidPlugin() {
+        // Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+        SceneManager.LoadScene("AnroidPlugin", LoadSceneMode.Single);
+    }
+
+    public void Quit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+}
